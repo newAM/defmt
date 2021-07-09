85e194eb9ef661bec1369c229966b981f84874a9
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2021-07-09 16:07:16 +0200

    add support for no-crate, level entries

--- macros/src/env_filter.rs
+++ macros/src/env_filter.rs
@@ -10,7 +10,9 @@ use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
 use syn::Ident;
 
+#[derive(Debug)]
 pub(crate) struct EnvFilter {
+    // key = module_path
     entries: BTreeMap<String, Level>,
 }
 
@@ -26,11 +28,12 @@ impl EnvFilter {
     fn new(defmt_log: Option<&str>, cargo_crate_name: &str) -> Self {
         // match `env_logger` behavior
         const LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED: Level = Level::Trace;
-        const LEVEL_WHEN_CRATE_IS_NOT_SPECIFIED: Level = Level::Error;
+        const LEVEL_WHEN_NOTHING_IS_SPECIFIED: Level = Level::Error;
 
         let caller_crate = cargo_crate_name;
 
         let mut entries = BTreeMap::new();
+        let mut fallback_level = None;
         if let Some(input) = defmt_log {
             for item in input.rsplit(',') {
                 let (module_path, level) = if let Some((module_path, level)) = item.rsplit_once('=')
@@ -38,8 +41,14 @@ impl EnvFilter {
                     let level = from_str(level).unwrap_or_else(|_| abort_call_site!("TODO"));
 
                     (module_path, level)
+                } else if let Ok(level) = from_str(item) {
+                    if fallback_level.is_none() {
+                        fallback_level = Some(level);
+                    }
+                    continue;
                 } else {
-                    (item, LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED)
+                    let module_path = item;
+                    (module_path, LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED)
                 };
 
                 validate_module_path(module_path);
@@ -51,7 +60,10 @@ impl EnvFilter {
         }
 
         if !entries.contains_key(caller_crate) {
-            entries.insert(caller_crate.to_string(), LEVEL_WHEN_CRATE_IS_NOT_SPECIFIED);
+            entries.insert(
+                caller_crate.to_string(),
+                fallback_level.unwrap_or(LEVEL_WHEN_NOTHING_IS_SPECIFIED),
+            );
         }
 
         EnvFilter { entries }
@@ -138,6 +150,10 @@ fn from_str(s: &str) -> Result<Level, ()> {
 }
 
 fn validate_module_path(path: &str) {
+    if path.is_empty() {
+        abort_call_site!("module path cannot be an empty string")
+    }
+
     for segment in path.split("::") {
         validate_identifier(segment)
     }
@@ -183,6 +199,34 @@ mod tests {
     }
 
     #[test]
+    fn when_only_level_is_specified_in_defmt_log_it_applies_to_all_crates() {
+        let env_filter = EnvFilter::new(Some("info"), "krate");
+        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Info));
+        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+    }
+
+    #[test]
+    fn moduleless_level_has_lower_precedence() {
+        let env_filter = EnvFilter::new(Some("krate=info,warn"), "krate");
+        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Info));
+        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+    }
+
+    #[test]
+    fn moduleless_level_behaves_like_a_krate_level_pair() {
+        let env_filter = EnvFilter::new(Some("krate::module=info,warn"), "krate");
+        assert_eq!(
+            btreeset!["krate", "krate::module"],
+            env_filter.paths_for_level(Level::Warn)
+        );
+        assert_eq!(
+            btreeset!["krate::module"],
+            env_filter.paths_for_level(Level::Info)
+        );
+        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+    }
+
+    #[test]
     fn module_paths_different_levels() {
         let env_filter = EnvFilter::new(Some("krate=info,krate::module=debug"), "krate");
         assert_eq!(
@@ -208,5 +252,11 @@ mod tests {
     #[should_panic]
     fn invalid_identifier() {
         EnvFilter::new(Some("krate::some-module"), "krate");
+    }
+
+    #[test]
+    #[should_panic]
+    fn reject_empty_module_path() {
+        EnvFilter::new(Some("=info"), "krate");
     }
 }
