4b6b0a9d19a10939cbb89af8a3ca0bc4830c0f9d
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2021-07-15 11:56:44 +0200

    refactor, bug fixes and tests
    - fixes `DEFMT_LOG=fooo=info` matching caller crate (CARGO_CRATE_NAME) `foo`
    - test that we accept raw identifiers in `DEFMT_LOG` (env_logger/RUST_LOG does)

--- macros/src/env_filter.rs
+++ macros/src/env_filter.rs
@@ -8,64 +8,61 @@ use std::panic as abort_call_site;
 use defmt_parser::Level;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::quote;
-use syn::Ident;
+
+use self::parse::{Entry, LogLevelOrOff, ModulePath};
+
+mod parse;
 
 #[derive(Debug)]
 pub(crate) struct EnvFilter {
-    // key = module_path
-    // level = None is used for the pseudo-level "off"
-    entries: BTreeMap<String, Option<Level>>,
+    // to keep the module paths sorted by length we use a btreemap
+    entries: BTreeMap<ModulePath, LogLevelOrOff>,
 }
 
 impl EnvFilter {
     pub(crate) fn from_env_var() -> Self {
         let defmt_log = env::var("DEFMT_LOG").ok();
-        let cargo_crate_name =
-            env::var("CARGO_CRATE_NAME").unwrap_or_else(|_| abort_call_site!("TODO"));
+        let cargo_crate_name = env::var("CARGO_CRATE_NAME")
+            .unwrap_or_else(|_| abort_call_site!("`CARGO_CRATE_NAME` env var is not set"));
 
         Self::new(defmt_log.as_deref(), &cargo_crate_name)
     }
 
     fn new(defmt_log: Option<&str>, cargo_crate_name: &str) -> Self {
         // match `env_logger` behavior
-        const LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED: Option<Level> = Some(Level::Trace);
-        const LEVEL_WHEN_NOTHING_IS_SPECIFIED: Option<Level> = Some(Level::Error);
+        const LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED: LogLevelOrOff = Some(Level::Trace);
+        const LEVEL_WHEN_NOTHING_IS_SPECIFIED: LogLevelOrOff = Some(Level::Error);
 
         let caller_crate = cargo_crate_name;
 
         let mut entries = BTreeMap::new();
-        let mut fallback_level = None;
+        let mut fallback_log_level = None;
         if let Some(input) = defmt_log {
-            for item in input.rsplit(',') {
-                let (module_path, level) = if let Some((module_path, level)) = item.rsplit_once('=')
-                {
-                    let level = from_str(level).unwrap_or_else(|_| abort_call_site!("TODO"));
-
-                    (module_path, level)
-                } else if let Ok(level) = from_str(item) {
-                    if fallback_level.is_none() {
-                        fallback_level = Some(level);
+            for entry in parse::defmt_log(input) {
+                let (modpath, level) = match entry {
+                    Entry::LogLevel(log_level) => {
+                        if fallback_log_level.is_none() {
+                            fallback_log_level = Some(log_level);
+                        }
+                        continue;
                     }
-                    continue;
-                } else {
-                    let module_path = item;
-                    (module_path, LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED)
+                    Entry::ModulePath(module) => (module, LEVEL_WHEN_LEVEL_IS_NOT_SPECIFIED),
+                    Entry::ModulePathLogLevel {
+                        module_path,
+                        log_level,
+                    } => (module_path, log_level),
                 };
 
-                validate_module_path(module_path);
-
-                if module_path.starts_with(&caller_crate) && !entries.contains_key(module_path) {
-                    entries.insert(module_path.to_string(), level);
+                if modpath.crate_name() == caller_crate && !entries.contains_key(&modpath) {
+                    entries.insert(modpath, level);
                 }
             }
         }
 
-        if !entries.contains_key(caller_crate) {
-            entries.insert(
-                caller_crate.to_string(),
-                fallback_level.unwrap_or(LEVEL_WHEN_NOTHING_IS_SPECIFIED),
-            );
-        }
+        let modpath = ModulePath::from_crate_name(caller_crate);
+        entries
+            .entry(modpath)
+            .or_insert_with(|| fallback_log_level.unwrap_or(LEVEL_WHEN_NOTHING_IS_SPECIFIED));
 
         EnvFilter { entries }
     }
@@ -75,7 +72,7 @@ impl EnvFilter {
     ///
     /// Returns `None` if the caller crate (at any module path) will never emit logs at requested log `level`
     pub(crate) fn path_check(&self, level: Level) -> Option<TokenStream2> {
-        let paths = self.paths_for_level(level);
+        let paths = self.modules(level);
 
         if paths.is_empty() {
             return None;
@@ -108,8 +105,8 @@ impl EnvFilter {
                         // end of module path _segment_
                         //
                         // `haystack` comes from `module_path!`; we assume it's well-formed so we
-                        // don't check *everything* that comes after `needle`; just a single character of
-                        // what should be the path separator ("::")
+                        // don't check *everything* that comes after `needle`; just the first
+                        // character of what should be the path separator ("::")
                         haystack[#needle_len] == b':'
                     }
                 })
@@ -126,7 +123,8 @@ impl EnvFilter {
         }))
     }
 
-    fn paths_for_level(&self, level: Level) -> BTreeSet<&str> {
+    /// Returns the set of modules that can emit logs at requested `level`
+    fn modules(&self, level: Level) -> BTreeSet<&str> {
         self.entries
             .iter()
             .rev()
@@ -144,80 +142,52 @@ impl EnvFilter {
     }
 }
 
-// TODO should be `impl FromStr for Level`
-fn from_str(s: &str) -> Result<Option<Level>, ()> {
-    Ok(Some(match s {
-        "debug" => Level::Debug,
-        "info" => Level::Info,
-        "error" => Level::Error,
-        "trace" => Level::Trace,
-        "warn" => Level::Warn,
-        "off" => return Ok(None),
-        _ => return Err(()),
-    }))
-}
-
-fn validate_module_path(path: &str) {
-    if path.is_empty() {
-        abort_call_site!("module path cannot be an empty string")
-    }
-
-    for segment in path.split("::") {
-        validate_identifier(segment)
-    }
-}
-
-fn validate_identifier(ident: &str) {
-    if syn::parse_str::<Ident>(ident).is_err() {
-        abort_call_site!("`{}` is not a valid identifier", ident)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use maplit::btreeset;
+    use pretty_assertions::assert_eq;
 
     use super::*;
 
     #[test]
     fn when_duplicates_entries_in_defmt_log_use_last_entry() {
         let env_filter = EnvFilter::new(Some("krate=info,krate=debug"), "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Debug));
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Trace));
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Debug));
+        assert_eq!(btreeset![], env_filter.modules(Level::Trace));
     }
 
     #[test]
     fn when_empty_defmt_log_use_error() {
         let env_filter = EnvFilter::new(None, "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Error));
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Warn));
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Error));
+        assert_eq!(btreeset![], env_filter.modules(Level::Warn));
     }
 
     #[test]
     fn when_no_level_in_defmt_log_use_trace() {
         let env_filter = EnvFilter::new(Some("krate"), "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Trace));
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Trace));
     }
 
     #[test]
     fn when_level_in_defmt_log_use_it() {
         let env_filter = EnvFilter::new(Some("krate=info"), "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Info));
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Info));
+        assert_eq!(btreeset![], env_filter.modules(Level::Debug));
     }
 
     #[test]
     fn when_only_level_is_specified_in_defmt_log_it_applies_to_all_crates() {
         let env_filter = EnvFilter::new(Some("info"), "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Info));
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Info));
+        assert_eq!(btreeset![], env_filter.modules(Level::Debug));
     }
 
     #[test]
     fn moduleless_level_has_lower_precedence() {
         let env_filter = EnvFilter::new(Some("krate=info,warn"), "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Info));
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Info));
+        assert_eq!(btreeset![], env_filter.modules(Level::Debug));
     }
 
     #[test]
@@ -225,13 +195,10 @@ mod tests {
         let env_filter = EnvFilter::new(Some("krate::module=info,warn"), "krate");
         assert_eq!(
             btreeset!["krate", "krate::module"],
-            env_filter.paths_for_level(Level::Warn)
+            env_filter.modules(Level::Warn)
         );
-        assert_eq!(
-            btreeset!["krate::module"],
-            env_filter.paths_for_level(Level::Info)
-        );
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
+        assert_eq!(btreeset!["krate::module"], env_filter.modules(Level::Info));
+        assert_eq!(btreeset![], env_filter.modules(Level::Debug));
     }
 
     #[test]
@@ -239,48 +206,38 @@ mod tests {
         let env_filter = EnvFilter::new(Some("krate=info,krate::module=debug"), "krate");
         assert_eq!(
             btreeset!["krate", "krate::module"],
-            env_filter.paths_for_level(Level::Info)
+            env_filter.modules(Level::Info)
         );
-        assert_eq!(
-            btreeset!["krate::module"],
-            env_filter.paths_for_level(Level::Debug)
-        );
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Trace));
+        assert_eq!(btreeset!["krate::module"], env_filter.modules(Level::Debug));
+        assert_eq!(btreeset![], env_filter.modules(Level::Trace));
     }
 
     #[test]
     fn blanket_off() {
         let env_filter = EnvFilter::new(Some("off"), "krate");
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Error));
+        assert_eq!(btreeset![], env_filter.modules(Level::Error));
     }
 
     #[test]
     fn blanket_off_plus_override() {
         let env_filter = EnvFilter::new(Some("krate::module=error,off"), "krate");
-        assert_eq!(
-            btreeset!["krate::module"],
-            env_filter.paths_for_level(Level::Error)
-        );
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Warn));
+        assert_eq!(btreeset!["krate::module"], env_filter.modules(Level::Error));
+        assert_eq!(btreeset![], env_filter.modules(Level::Warn));
     }
 
-    #[ignore = "TODO(optimization): impl & more test cases"]
+    #[test]
+    fn does_not_match_partial_crate_name() {
+        let env_filter = EnvFilter::new(Some("fooo=warn"), "foo");
+        assert_eq!(btreeset!["foo"], env_filter.modules(Level::Error));
+        assert_eq!(btreeset![], env_filter.modules(Level::Warn));
+    }
+
+    // doesn't affect runtime performance but it makes the expanded code smaller
+    #[ignore = "TODO(P-low/optimization): impl & more test cases"]
     #[test]
     fn when_module_paths_with_same_level_remove_inner_ones() {
         let env_filter = EnvFilter::new(Some("krate=info,krate::module=info"), "krate");
-        assert_eq!(btreeset!["krate"], env_filter.paths_for_level(Level::Info));
-        assert_eq!(btreeset![], env_filter.paths_for_level(Level::Debug));
-    }
-
-    #[test]
-    #[should_panic]
-    fn invalid_identifier() {
-        EnvFilter::new(Some("krate::some-module"), "krate");
-    }
-
-    #[test]
-    #[should_panic]
-    fn reject_empty_module_path() {
-        EnvFilter::new(Some("=info"), "krate");
+        assert_eq!(btreeset!["krate"], env_filter.modules(Level::Info));
+        assert_eq!(btreeset![], env_filter.modules(Level::Debug));
     }
 }
